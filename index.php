a9da8d8999449e887b20059976ff78f23939fda1
Connor Kennedy <ckennedy733@qub.ac.uk>
2020-01-12 01:23:25 +0000

    Please work

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
     <body>
     <div style="width: 600px; margin: 10px auto">
     <h1>Search the web with Connor's Search</h1>
-    <form action ="search.php" method="post">
+    <form action ="crawler.php" method="post">
     <input type="text" style="width: 100%; font-size: 16px;" name="q" value="" placeholder="Search the web . . .">
-    <div style="text-allign: center"><input type="submit" style="font-size: 16px;" name="search" value="Search">
+    <div style="text-allign: center"><input type="submit" style="font-size: 16px;" name="search" value="Crawl">
     </div>
     </form>
     </body>

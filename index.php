2d00cba0cc9a207645bf389e2b4b8f8aeea38dc2
Connor Kennedy <ckennedy733@qub.ac.uk>
2020-01-12 00:00:26 +0000

    Commit for Github REPO

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Connors Search Engine</title>
+    </head>
+    <body>
+    <div style="width: 600px; margin: 10px auto">
+    <h1>Search the web with Connor's Search</h1>
+    <form action ="search.php" method="post">
+    <input type="text" style="width: 100%; font-size: 16px;" name="q" value="" placeholder="Search the web . . .">
+    <div style="text-allign: center"><input type="submit" style="font-size: 16px;" name="search" value="Search">
+    </div>
+    </form>
+    </body>
+    </html>
---
+++ search.php
@@ -0,0 +1,44 @@
+<?php
+
+$pdo = new PDO('sqlsrv:server=tcp:40179436dbserver.database.windows.net,1433;Database=searchEngine','dbmanager40179436','ySLH33CI8');
+
+$search = $_GET['q'];
+
+$searche = explode(" ", $search);
+
+$x =0;
+$construct = "";
+$params = array();
+
+foreach ($searche as $term) {
+	$x++;
+	if ($x == 1) {
+		$construct .= "title LIKE CONCAT('%',:search$x,'%') OR description LIKE CONCAT('%',:search$x,'%') OR keywords LIKE CONCAT('%',:search$x,'%')";
+	} else {
+		$construct .= "AND title LIKE CONCAT('%',:search$x,'%') OR description LIKE CONCAT('%',:search$x,'%') OR keywords LIKE CONCAT('%',:search$x,'%')";
+	}
+	$params["search$x"] = $term;
+}
+
+$results = $pdo->prepare("SELECT * FROM 'index' WHERE $construct");
+$results->execute($params);
+
+
+if ($results->rowCount() == 0) {
+	echo "0 results found! <hr />";
+} else {
+	echo $results->rowCount()." results found! <hr />";
+}
+
+//echo "<pre>";
+foreach ($results->fetchAll() as $result) {
+	echo $result["title"]."<br />";
+	if ($result["description"] == "") {
+		echo "No description available."."<br />";
+	} else {
+	echo $result["description"]."<br />";
+	}
+	echo $result["url"]."<br />";
+	echo "<hr />";
+}
+//print_r($results->fetchAll());
